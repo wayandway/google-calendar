be17408b275aa26baba442203a05ea22e2e198e2
Jiyun Kim <wayandway@kakao.com>
2025-06-13 17:14:52 +0900

    refactor: 라우팅 제거 및 한 페이지(/calendar)에서 월/주간 달력 뷰 전환하도록 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,11 @@
-import { redirect } from 'next/navigation';
+import React from 'react';
 
-export default function Home() {
-  redirect('/month');
+import MainCalendar from '@/components/main-calendar/MainCalendar';
+
+export default function CalendarPage() {
+  return (
+    <div className="h-full">
+      <MainCalendar />
+    </div>
+  );
 }
